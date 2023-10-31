a1bedaa79fa99b99171688535c2e5d0a3c8e5ab0
Jeffrey Interiano <jeff.interiano@outlook.com>
2023-10-31 12:00:20 -0400

    first commit

---
+++ Calculator/display.js
@@ -0,0 +1,17 @@
+// JavaScript functions
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    try {
+        var result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
